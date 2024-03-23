78888ffc8da1ee002e95c727291c031fe98ffd30
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-03-23 14:35:31 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+    $(".btn1").click(function() {
+        
+        $(".answerOne").toggle()
+        $(".plusOne").toggle();
+        $(".minusOne").toggle();
+
+
+    })
+    $(".btn2").click(function() {
+        
+        $(".answerTwo").toggle()
+        $(".plusTwo").toggle();
+        $(".minusTwo").toggle();
+
+    })
+    $(".btn3").click(function() {
+        
+        $(".answerThree").toggle()
+        $(".plusThree").toggle();
+        $(".minusThree").toggle();
+
+    })
+    $(".btn4").click(function() {
+        
+        $(".answerFour").toggle()
+        $(".plusFour").toggle();
+        $(".minusFour").toggle();
+
+    })
+
+
+})
